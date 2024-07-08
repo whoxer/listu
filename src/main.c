d2f8c4d44c8cf1d6c4e339f6d1eb4177e56c01ee
whoxer <natanaelvsant@gmail.com>
2024-07-08 18:53:27 -0300

    Arquivo main do programa

---
+++ src/main.c
@@ -0,0 +1,21 @@
+#include "tasks.h"
+
+int main(void) {
+	task *STACK = (task *) malloc(sizeof(task));
+
+	if (!STACK) {
+		printf("Sem memória disponível.");
+		exit(1);
+	} else {
+		init(STACK);
+		int opt;
+
+		do {
+			opt = menu();
+			option(STACK, opt);
+		} while (opt)
+
+		free(STACK);
+	}
+	return 0;
+}
