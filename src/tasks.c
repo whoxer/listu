1940b3228a3900bfa19ea1aa40fc41561be42ef1
whoxer <natanaelvsant@gmail.com>
2024-07-11 18:12:47 -0300

    Função pop(), push(), menu() e free_stack() implementadas

--- src/tasks.c
+++ src/tasks.c
@@ -2,6 +2,9 @@
 #include "tasks.h"
 #include "alloc.h"
 
+// Tamanho da pilha
+int size;
+
 // Inicia pilha de tarefas com valor zero e aponta ponteiro da pilha para NULO.
 void init(task *STACK) {
 	STACK->next = NULL;
@@ -9,7 +12,16 @@ void init(task *STACK) {
 }
 
 int menu(void) {
-	// code
+	int opt;
+
+	printf(" tasks.c, v0.0.1\n escrito por %s\n\n", AUTHOR);
+	printf(" [1] criar nova tarefa\n"); //push
+	printf(" [2] exibir todas as tarefas\n"); //show
+	printf(" [3] excluir ultima tarefa\n"); //pop
+	printf(" [4] excluir todas as tarefas\n");//free
+	printf(" [5] sair");
+
+	return opt;
 }
 
 // TODO Programar depois das funções que manipulam as pilhas.
@@ -52,6 +64,52 @@ void show(task *STACK) {
 	printf("\n\n");
 }
 
-void free(stack *STACK) {
+void free_stack(task *STACK) {
+	if (!isit_void(STACK)) {
+		task *nextTask,
+		     *current;
 
+		current = STACK->next;
+		while(current != NULL) {
+			nextTask = current->next;
+			free(current);
+			current = nextTask;
+		}
+	}
+}
+
+void push(task *STACK) {
+	task *new = alloc();
+	new->next = NULL;
+
+	if (isit_void(STACK)) {
+		STACK->next = new;
+	} else {
+		task *tmp = STACK->next;
+
+		while (tmp->next != NULL) {
+			tmp = tmp->next;
+
+			tmp->next = new;
+		}
+	}
+	size++;
+}
+
+task *pop(task *STACK) {
+	if (STACK->next == NULL) {
+		printf("Lista de tarefas vazia.\n\n");
+		return NULL;
+	} else {
+		task *last = STACK->next,
+		     *penult = STACK;
+
+		while (last->next != NULL) {
+			penult = last;
+			last = last->next;
+		}
+		penult->next = NULL;
+		size--;
+		return last;
+	}
 }
