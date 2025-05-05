afaf35c75fa8679f5907c88b6c8cd5540a34af1b
whoxer <natanaelvsant@gmail.com>
2025-05-05 10:56:42 -0300

    Commit inicial do App desktop

---
+++ app-desktop/renderer.js
@@ -0,0 +1,8 @@
+
+
+function createNewListu() {
+    title = document.getElementById('title').value
+    description = document.getElementById('description').value
+    
+    document.getElementById('list-listu').innerText = title
+}
