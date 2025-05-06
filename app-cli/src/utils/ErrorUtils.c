94ced2862c61cabd099b309ff8515b24ae82bd13
whoxer <natanaelvsant@gmail.com>
2025-05-06 07:30:17 -0300

    Definição e implementação de melhores tratamentos de erro em tempo de execução

--- app-cli/src/utils/ErrorUtils.c
+++ app-cli/src/utils/ErrorUtils.c
@@ -38,3 +38,38 @@ error throw_invalid()
 
     return "** ";
 }
+
+error err_to_create_dir(){
+    load_config();
+
+    if (strcmp(language(), "pt-br") == 0)
+        return "Erro ao criar diretório .listu";
+    if (strcmp(language(), "en") == 0)
+        return "Error creating .listu directory";
+
+    return "** ";
+}
+
+error acess_dir()
+{
+    load_config();
+
+    if (strcmp(language(), "pt-br") == 0)
+        return "Erro ao acessar o diretório .listu *** Possível solução:\n\n\t> listu init\n\n";
+    if (strcmp(language(), "en") == 0)
+        return "Error accessing .listu directory *** Possible solution:\n\n\t> listu init\n\n";
+
+    return "** ";
+}
+
+error err_to_acess_file()
+{
+    load_config();
+
+    if (strcmp(language(), "pt-br") == 0)
+        return "Erro ao verificar ou abrir arquivos. Arquivo pode não ter sido criado ou nome pode estar incorreto.";
+    if (strcmp(language(), "en") == 0)
+        return "Error checking or opening files. File may not have been created or name may be incorrect.";
+
+    return "** ";
+}
